49991ad77c7ff046d5e255bb99461f2aa66c3241
Luisfetoga2 <l.torresg@uniandes.edu.co>
2024-09-18 09:19:34 -0500

    Location request screen

--- lib/views/map_screen.dart
+++ lib/views/map_screen.dart
@@ -10,85 +10,104 @@ class MapScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (_) => MapViewModel(),
-      child: Stack(
-        children: [
-          Consumer<MapViewModel>(
-            builder: (context, viewModel, child) {
-              if (!viewModel.state.permissionsGranted) {
-                return const Center(child: CircularProgressIndicator());
-              }
-              return GoogleMap(
-                initialCameraPosition: CameraPosition(
-                  target: viewModel.state.startLocation,
-                  zoom: 15.0,
-                ),
-                myLocationEnabled: viewModel.state.permissionsGranted,
-                myLocationButtonEnabled: viewModel.state.permissionsGranted,
-                circles: {
-                  Circle(
-                    circleId: const CircleId('circle'),
-                    center: viewModel.state.circleLocation,
-                    radius: viewModel.state.circleRadius * 1000, // Convert km to meters
-                    fillColor: Colors.blue.withOpacity(0.2),
-                    strokeColor: Colors.blue,
-                    strokeWidth: 1,
-                  ),
-                },
-                buildingsEnabled: false,
-                // Add markers for restaurants if needed
-              );
-            },
-          ),
-          Positioned(
-            bottom: 10,
-            left: 10,
-            right: 10,
-            child: Consumer<MapViewModel>(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('MapScreen'),
+        ),
+        body: Stack(
+          children: [
+            Consumer<MapViewModel>(
               builder: (context, viewModel, child) {
-                return Container(
-                  padding: const EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(8.0),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.black26,
-                        blurRadius: 4.0,
-                        offset: Offset(0, 2),
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0), // Adjust the padding as needed
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const Text('Search range:'),
-                            Text('${viewModel.state.circleRadius.toStringAsFixed(1)} km'),
-                          ],
+                if (!viewModel.state.permissionsGranted) {
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Access to location has not been granted.',
+                          style: TextStyle(fontSize: 16.0),
                         ),
-                      ),
-                      Slider(
-                        value: viewModel.state.circleRadius,
-                        min: 0.1,
-                        max: 1.5,
-                        divisions: 99,
-                        label: viewModel.state.circleRadius.toStringAsFixed(1),
-                        onChanged: (value) {
-                          viewModel.updateCircleRadius(value);
-                        },
-                        activeColor: const Color(0xFFD9534F),
-                        inactiveColor: const Color(0xFFFFEEAD),
-                      ),
-                    ],
+                        const SizedBox(height: 16.0),
+                        ElevatedButton(
+                          onPressed: viewModel.openAppSettings, // Call ViewModel method
+                          child: const Text('Open App Settings'),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+                return GoogleMap(
+                  initialCameraPosition: CameraPosition(
+                    target: viewModel.state.startLocation,
+                    zoom: 15.0,
                   ),
+                  myLocationEnabled: viewModel.state.permissionsGranted,
+                  myLocationButtonEnabled: viewModel.state.permissionsGranted,
+                  circles: {
+                    Circle(
+                      circleId: const CircleId('circle'),
+                      center: viewModel.state.circleLocation,
+                      radius: viewModel.state.circleRadius * 1000, // Convert km to meters
+                      fillColor: Colors.blue.withOpacity(0.5),
+                      strokeColor: Colors.blue,
+                      strokeWidth: 1,
+                    ),
+                  },
+                  // TODO Add markers for each restaurant
                 );
               },
             ),
-          ),
-        ],
+            Positioned(
+              bottom: 10,
+              left: 10,
+              right: 10,
+              child: Consumer<MapViewModel>(
+                builder: (context, viewModel, child) {
+                  return Container(
+                    padding: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8.0),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 4.0,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text('Search range:'),
+                              Text('${viewModel.state.circleRadius.toStringAsFixed(1)} km'),
+                            ],
+                          ),
+                        ),
+                        Slider(
+                          value: viewModel.state.circleRadius,
+                          min: 0.1,
+                          max: 1.5,
+                          divisions: 99,
+                          label: viewModel.state.circleRadius.toStringAsFixed(1),
+                          onChanged: (value) {
+                            viewModel.updateCircleRadius(value);
+                          },
+                          activeColor: const Color(0xFFD9534F),
+                          inactiveColor: const Color(0xFFFFEEAD),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
