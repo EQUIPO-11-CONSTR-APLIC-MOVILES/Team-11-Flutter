5bdc30f56137b25524bcc0390c8157215f70c02b
Luisfetoga2 <l.torresg@uniandes.edu.co>
2024-10-02 13:42:38 -0500

    NavBar fixed

--- lib/views/navigator_screen.dart
+++ lib/views/navigator_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';  // Add provider package
 import 'package:restau/viewmodels/log_in_viewmodel.dart';
 import 'package:restau/viewmodels/user.dart';
 import '../viewmodels/navigator_viewmodel.dart'; 
@@ -12,15 +13,13 @@ class NavigatorScreen extends StatefulWidget {
   const NavigatorScreen({super.key});
 
   @override
-  _NavigatorScreenState createState() => _NavigatorScreenState();
+  NavigatorScreenState createState() => NavigatorScreenState();
 }
 
-class _NavigatorScreenState extends State<NavigatorScreen> {
-  int _selectedIndex = 0;
+class NavigatorScreenState extends State<NavigatorScreen> {
   
   User activeUser = User();
   LogInViewmodel livm = LogInViewmodel();
-  NavigatorViewModel vm = NavigatorViewModel();
 
   static final List<Widget> _widgetOptions = <Widget>[
     const HomeScreen(),
@@ -32,76 +31,88 @@ class _NavigatorScreenState extends State<NavigatorScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0), // Right inset for the avatar
-            child: FutureBuilder<String?>(
-              future: activeUser.getUserPic(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  // While loading, show a circular progress indicator
-                  return const CircularProgressIndicator();
-                }
-                if (snapshot.hasError) {
-                  // Handle error (could be null if user is not logged in)
-                  return const Icon(Icons.error);
-                }
+    // Wrap the whole widget with ChangeNotifierProvider if not already done at a higher level
+    return ChangeNotifierProvider(
+      create: (context) => NavigatorViewModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: FutureBuilder<String?>(
+                future: activeUser.getUserPic(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  }
+                  if (snapshot.hasError) {
+                    return const Icon(Icons.error);
+                  }
 
-                // Get the profile picture URL
-                String? profilePicUrl = snapshot.data;
+                  String? profilePicUrl = snapshot.data;
 
-                return GestureDetector(
-                  onTap: () => _showLogoutMenu(context),
-                  child: CircleAvatar(
-                    backgroundImage: profilePicUrl != null
-                        ? NetworkImage(profilePicUrl)
-                        : null, // Default or placeholder image can be added here
-                    radius: 20, // Adjust radius as needed
-                    child: profilePicUrl == null
-                        ? const Icon(Icons.person) // Default icon if no image
-                        : null,
-                  ),
-                );
-              },
+                  return GestureDetector(
+                    onTap: () => _showLogoutMenu(context),
+                    child: CircleAvatar(
+                      backgroundImage: profilePicUrl != null
+                          ? NetworkImage(profilePicUrl)
+                          : null,
+                      radius: 20,
+                      child: profilePicUrl == null
+                          ? const Icon(Icons.person)
+                          : null,
+                    ),
+                  );
+                },
+              ),
             ),
-          ),
-        ],
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(1.0), // Height of the bottom border
-          child: Container(
-            color: Colors.black, // Black color for the bottom line
-            height: 1.0, // Thickness of the bottom border
+          ],
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(1.0),
+            child: Container(
+              color: Colors.black,
+              height: 1.0,
+            ),
           ),
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 16.0),
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: List.generate(5, (index) {
-          return BottomNavigationBarItem(
-            icon: Container(
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: _selectedIndex == index ? const Color(0xFFFFEEAD) : Colors.transparent,
-              ),
-              padding: const EdgeInsets.all(8.0), 
-              child: Icon(
-                vm.getIconForIndex(index),
-                color: _selectedIndex == index ? Colors.black : Colors.black54,
-              ),
-            ),
-            label: '',
-          );
-        }),
-        currentIndex: _selectedIndex,
-        onTap: vm.onItemTapped,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
+        body: Padding(
+          padding: const EdgeInsets.only(top: 16.0),
+          // Listen to changes in the selectedIndex from the NavigatorViewModel
+          child: Consumer<NavigatorViewModel>(
+            builder: (context, vm, child) {
+              return _widgetOptions.elementAt(vm.selectedIndex);
+            },
+          ),
+        ),
+        bottomNavigationBar: Consumer<NavigatorViewModel>(
+          builder: (context, vm, child) {
+            return BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              items: List.generate(5, (index) {
+                return BottomNavigationBarItem(
+                  icon: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: vm.selectedIndex == index
+                          ? const Color(0xFFFFEEAD)
+                          : Colors.transparent,
+                    ),
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(
+                      vm.getIconForIndex(index),
+                      color: Colors.black,
+                    ),
+                  ),
+                  label: '',
+                );
+              }),
+              currentIndex: vm.selectedIndex,
+              onTap: vm.onItemTapped,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+            );
+          },
+        ),
       ),
     );
   }
@@ -109,14 +120,14 @@ class _NavigatorScreenState extends State<NavigatorScreen> {
   void _showLogoutMenu(BuildContext context) {
     showMenu(
       context: context,
-      position: RelativeRect.fromLTRB(100, 80, 0, 0), // Adjust the position as needed
+      position: const RelativeRect.fromLTRB(100, 80, 0, 0),
       items: [
         const PopupMenuItem<String>(
           value: 'logout',
           child: Row(
             children: [
-              Icon(Icons.logout), // Logout icon
-              SizedBox(width: 8), // Space between icon and text
+              Icon(Icons.logout),
+              SizedBox(width: 8),
               Text('Log Out'),
             ],
           ),
