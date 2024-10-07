30ead27be50267de48daed15ccbc6511ef4f4200
Luisfetoga2 <l.torresg@uniandes.edu.co>
2024-10-07 16:06:54 -0500

    Implemented: Is there a new restaurant nearby that matches the user's 'liked' cuisine types?

--- lib/navigation/navigator_viewmodel.dart
+++ lib/navigation/navigator_viewmodel.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:restau/models/firestore_service.dart';
+import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
+import 'package:restau/navigation/user_viewmodel.dart';
+
+// Global variable to track if the location has been sent
+bool _locationSent = false;
 
 class NavigatorViewModel extends ChangeNotifier {
 
@@ -7,9 +13,11 @@ class NavigatorViewModel extends ChangeNotifier {
   final FirestoreService _firestoreService = FirestoreService();
   String navigationPath = 'Home'; // Initial path is Home
   late String sessionId;
+  String apiMessage = '';
 
   NavigatorViewModel() {
     initializeSessionId();
+    _checkLocationPermission();
   }
 
   void initializeSessionId() async {
@@ -65,5 +73,85 @@ class NavigatorViewModel extends ChangeNotifier {
       default:
         return Icons.home; // Fallback icon
     }
+  }
+
+  LocationData? _currentLocation;
+  UserViewModel activeUser = UserViewModel();
+
+  Future<void> _checkLocationPermission() async {
+    print('Checking location permission');
+    final location = Location();
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _currentLocation = await location.getLocation();
+    if (_currentLocation != null && !_locationSent) {
+      _sendLocationToApi();
+    }
+  }
+
+  Future<void> _sendLocationToApi() async {
+    print('Sending location to API');
+    try {
+      final userInfo = await activeUser.getUserInfo();
+      final userID = userInfo?['uid'];
+      final lat = _currentLocation!.latitude;
+      final lon = _currentLocation!.longitude;
+
+      String apiURL = "http://34.134.5.98:8000";
+
+      print("fetching");
+      final response = await http
+        .get(
+          Uri.parse(
+            '$apiURL/nearbyxcuisine?userID=lPS20gyuL2SeBaf5tmiy4pVlnLK2&lat=4.6733676&lon=-74.1012443'),
+            //'$apiURL/nearbyxcuisine?userID=$userID&lat=$lat&lon=$lon'),
+        )
+        .timeout(const Duration(seconds: 5)); // 10-second timeout
+
+      print("Fetched");
+
+      if (response.statusCode == 200) {
+        final responseBody = response.body.replaceAll('"', "");
+
+        print(responseBody);
+        if (responseBody != 'null') {
+          // Convert string
+          final firstChar = responseBody.isNotEmpty ? responseBody[0].toLowerCase() : '';
+          final message = ['a', 'e', 'i', 'o', 'u'].contains(firstChar)
+              ? "There's an $responseBody restaurant near you"
+              : "There's a $responseBody restaurant near you";
+
+          apiMessage = message;
+          _locationSent = true; // Set the flag to true after sending location
+        }
+        
+      } else {
+        print('Failed to send location to API');
+        print("Response status: ${response.statusCode}");
+        //apiMessage = 'Failed to get restaurant';
+      }
+    } catch (e) {
+      print('Failed to send location to API');
+      print(e);
+      //apiMessage = 'Failed to fetch';
+    }
+    notifyListeners();
   }
 }
